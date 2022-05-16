31fcdaa711d31eba330f3281291becf15c63c00d
Piotr Walacik <piotr.walacik@hotmail.com>
2022-05-16 23:21:46 +0200

    RestClient

--- test/ReportManagement.Api.Test.Acceptance/API/ReportManagementApi.cs
+++ test/ReportManagement.Api.Test.Acceptance/API/ReportManagementApi.cs
@@ -1,7 +1,18 @@
-﻿namespace ReportManagement.Api.Test.Acceptance.API
+﻿using RestSharp;
+using System.Net;
+
+namespace ReportManagement.Api.Test.Acceptance.API
 {
     internal class ReportManagementApi
     {
         private readonly RestClient _client;
+
+        public ReportManagementApi()
+        {
+            _client = new RestClient("https://localhost:5001");
+
+            ServicePointManager.ServerCertificateValidationCallback +=
+                (sender, cert, chain, sslPolicyErrors) => true;
+        }
     }
 }
